b2c06830817f43ccf395d7128c0f67bc939b0e9d
JordanR <jar5927@rit.edu>
2022-11-14 17:24:06 -0500

    setting up repo

---
+++ server/config.js
@@ -0,0 +1,25 @@
+// read in all the .env inog
+require('dotenv').config();
+
+const connections = {
+    development :{
+        http: {
+            port: process.env.PORT || process.env.NODE_PORT || 3000,
+        },
+        mongo: process.env.MONGODB_URI || 'mongodb://127.0.0.1/project2Database',
+        redis: process.env.REDISCLOUD_URL,
+    },
+    production: {
+        http: {
+            port: process.env.PORT || process.env.NODE_PORT || 3000,
+        },
+        mongo: process.env.MONGODB_URI,
+        redis: process.env.REDISCLOUD_URL,
+    },
+}
+
+
+module.exports = {
+    connections: connections[process.env.NODE_ENV],
+    secret: process.env.SECRET,
+}
